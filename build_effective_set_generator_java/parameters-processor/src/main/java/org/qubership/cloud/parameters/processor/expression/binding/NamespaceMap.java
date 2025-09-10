a3eea39159b2ed59a20c22b2aae15a40e1097271
Siva Reddy Kunduru <35566000+sivareddyit@users.noreply.github.com>
2025-09-10 21:27:14 +0530

    fix: fixed issues with namepsace and also fixed the BASELINE_PROJ, BASELINE_ORIGIN, PRIVATE_IDENTITY_PROVIDER_URL (#580)
    Co-authored-by: siva <sivareddyit@gmail.com>

--- build_effective_set_generator_java/parameters-processor/src/main/java/org/qubership/cloud/parameters/processor/expression/binding/NamespaceMap.java
+++ build_effective_set_generator_java/parameters-processor/src/main/java/org/qubership/cloud/parameters/processor/expression/binding/NamespaceMap.java
@@ -83,25 +83,111 @@ public class NamespaceMap extends DynamicMap {
 
                 // Deprecated deployer parameters
                 map.putIfAbsent(GATEWAY_URL, "http://internal-gateway-service:8080");
-                map.putIfAbsent(STATIC_CACHE_SERVICE_ROUTE_HOST, String.format("static-cache-service-%s.%s", namespaceName, cloudHostname));
+                map.putIfAbsent(STATIC_CACHE_SERVICE_ROUTE_HOST, String.format("static-cache-service-%s.%s", originalNamespace, cloudHostname));
 
                 CredentialUtils credentialUtils = Injector.getInstance().getDi().get(CredentialUtils.class);
 
-                map.put(ORIGIN_NAMESPACE, originalNamespace);
+
+                if (bgDomainEntityDTO != null) {
+
+                    BgDomainEntityDTO.NamespaceDTO origin = bgDomainEntityDTO.getOriginNamespace();
+                    BgDomainEntityDTO.NamespaceDTO peer = bgDomainEntityDTO.getPeerNamespace();
+                    BgDomainEntityDTO.NamespaceDTO controller = bgDomainEntityDTO.getController();
+
+                    //Primary Namespace & Secondary Namespace
+                    if (origin.getName().equalsIgnoreCase(originalNamespace)) {
+                        map.put(ORIGIN_NAMESPACE, originalNamespace);
+                        map.put(PEER_NAMESPACE, peer.getName());
+                        map.put(CONTROLLER_NAMESPACE, controller.getName());
+                    } else if (controller.getName().equalsIgnoreCase(originalNamespace)) { //Controller Namespace
+                        if (origin != null) {
+                            map.put(ORIGIN_NAMESPACE, origin.getName());
+                        } else {
+                            map.put(ORIGIN_NAMESPACE, originalNamespace);
+                        }
+                        map.put(PEER_NAMESPACE, peer.getName());
+                        map.put(CONTROLLER_NAMESPACE, controller.getName());
+
+                        if (controller != null) {
+                            if (controller.getUrl() != null && !controller.getUrl().isEmpty()) {
+                                map.put(BG_CONTROLLER_URL, controller.getUrl());
+                            } else {
+                                String bg_url = String.format("%s://bluegreen-controller-%s.%s", protocol.toLowerCase(), originalNamespace, customHost);
+                                map.put(BG_CONTROLLER_URL, bg_url);
+                            }
+
+                            if (controller.getCredentials() != null && !controller.getCredentials().isEmpty()) {
+                                Credential credentialPojo = credentialUtils.getCredentialsById(controller.getCredentials());
+                                if (credentialPojo instanceof UsernamePasswordCredentials) {
+                                    UsernamePasswordCredentials usernamePasswordCredentials = (UsernamePasswordCredentials) credentialPojo;
+                                    map.put(BG_CONTROLLER_LOGIN, usernamePasswordCredentials.getUsername());
+                                    map.put(BG_CONTROLLER_PASSWORD, usernamePasswordCredentials.getPassword());
+                                }
+                            } else {
+                                map.put(BG_CONTROLLER_LOGIN, "bgoperator");
+                                map.put(BG_CONTROLLER_PASSWORD, "F21wuZNRpw");
+                            }
+                        }
+
+//                        String rootUrl = Injector.getInstance().get(URLUtils.class).getRootUrl();
+//                        rootUrl = rootUrl.endsWith("/") ? rootUrl.substring(0, rootUrl.length() - 1) : rootUrl;
+                        map.put(CMDB_CALLBACK_TOKEN, "");
+                        map.put(CMDB_CALLBACK_URL, "");
+//                        map.put(CMDB_CALLBACK_TOKEN, Injector.getInstance().get(BGDomainRepository.class).getBgDomainToken(tenant, cloud, config.getBgDomainNamespaceData().getName()));
+//                        map.put(CMDB_CALLBACK_URL, String.format("%s/cm/v1/tenants/%s/clouds/%s/bgdomains/clone",
+//                                rootUrl, tenant, cloud));
+                    }
+                } else {
+                    map.put(ORIGIN_NAMESPACE, originalNamespace);
+                }
+
+                if (compositeStructureDTO != null) {
+                    setBaselineVars(map, compositeStructureDTO.getBaseline(), bgDomainEntityDTO);
+                } else if (bgDomainEntityDTO != null && compositeStructureDTO != null) {
+                    if (bgDomainEntityDTO.getType().equalsIgnoreCase(BG_DOMAIN)) {
+                        setBaselineVars(map, compositeStructureDTO.getBaseline(), bgDomainEntityDTO);
+                    }
+
+                }
 
                 // Deprecated deployer parameters
                 map.putIfAbsent(GATEWAY_URL, "http://internal-gateway-service:8080");
-                map.putIfAbsent(STATIC_CACHE_SERVICE_ROUTE_HOST, String.format("static-cache-service-%s.%s", namespaceName, cloudHostname));
+                map.putIfAbsent(STATIC_CACHE_SERVICE_ROUTE_HOST, String.format("static-cache-service-%s.%s", originalNamespace, cloudHostname));
+
+                String gatewayNamespace = "";
+                String idpUrlNamespace = "";
+
+                if (bgDomainEntityDTO != null) {
+                    if (bgDomainEntityDTO.getType().equalsIgnoreCase(BG_DOMAIN)) {
+                        gatewayNamespace = bgDomainEntityDTO.getOriginNamespace().getName();
+                        idpUrlNamespace = bgDomainEntityDTO.getName();
+                    } else {
+                        String originNamespaceName = bgDomainEntityDTO.getOriginNamespace().getName();
+                        gatewayNamespace = originNamespaceName;
+                        idpUrlNamespace = originNamespaceName;
+                    }
+                } else if (compositeStructureDTO != null) {
+                    CompositeEntityDTO compositeEntityBase = compositeStructureDTO.getBaseline();
+                    String namespaceFromBaseline = compositeEntityBase.getName();
+                    gatewayNamespace = originalNamespace;
+                    idpUrlNamespace = originalNamespace;
+                    if(!originalNamespace.equalsIgnoreCase(compositeEntityBase.getName())){
+                        gatewayNamespace = originalNamespace;
+                        idpUrlNamespace = namespaceFromBaseline;
+                    }
+                } else {
+                    gatewayNamespace = originalNamespace;
+                    idpUrlNamespace = originalNamespace;
+                }
 
                 // Deployer parameters
-                addGatewayIdentityUrls(config.getCustomParameters(), map, false, protocol.toLowerCase(), customHost, namespaceName, namespaceName);
-                addGatewayIdentityUrls(config.getCustomParameters(), map, true, protocol.toLowerCase(), cloudHostname, namespaceName, namespaceName);
-                map.putIfAbsent(NAMESPACE, namespaceName);
+                addGatewayIdentityUrls(config.getCustomParameters(), map, false, protocol.toLowerCase(), customHost, gatewayNamespace, idpUrlNamespace);
+                addGatewayIdentityUrls(config.getCustomParameters(), map, true, protocol.toLowerCase(), cloudHostname, gatewayNamespace, idpUrlNamespace);
                 map.putIfAbsent(SSL_SECRET, "defaultsslcertificate");
                 map.putIfAbsent(BUILD_TAG_NEW, "keycloak-database");
                 if (binding.getDeployerInputs() != null) {
-                    map.put("CLIENT_PREFIX",namespaceName);
-                    if ( binding.getDeployerInputs().getSecretId() != null) {
+                    map.put("CLIENT_PREFIX", originalNamespace);
+                    if (binding.getDeployerInputs().getSecretId() != null) {
                         setSecretValues(map, credentialUtils);
                     }
 //                    if (binding.getDeployerInputs().getDeploySessionId() != null) {
@@ -125,6 +211,21 @@ public class NamespaceMap extends DynamicMap {
             return map;
         }
         return null;
+    }
+
+    private void setBaselineVars(EscapeMap map, CompositeEntityDTO baselineEntity, BgDomainEntityDTO bgDomainEntityDTO) {
+        if (baselineEntity != null) {
+            if (baselineEntity.getType().equalsIgnoreCase(BG_DOMAIN) && bgDomainEntityDTO != null) {
+                map.put(BASELINE_ORIGIN, bgDomainEntityDTO.getOriginNamespace().getName());
+                map.put(BASELINE_PEER, bgDomainEntityDTO.getPeerNamespace().getName());
+                map.put(BASELINE_CONTROLLER, bgDomainEntityDTO.getController().getName());
+                map.put(BASELINE_PROJ, bgDomainEntityDTO.getController().getName());
+            } else if (baselineEntity.getType().equalsIgnoreCase(NAMESPACE) &&
+                    !baselineEntity.getName().equalsIgnoreCase(originalNamespace)) {
+                map.put(BASELINE_ORIGIN, baselineEntity.getName());
+                map.put(BASELINE_PROJ, baselineEntity.getName());
+            }
+        }
     }
 
     private void setSecretValues(EscapeMap map, CredentialUtils credentialUtils) {
