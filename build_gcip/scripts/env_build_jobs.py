5b87522f5014124185a822bcb7c205e357aa0838
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2025-09-10 14:25:42 +0300

    feat: Added RUNNER_SCRIPT_TIMEOUT

--- build_gcip/scripts/env_build_jobs.py
+++ build_gcip/scripts/env_build_jobs.py
@@ -48,6 +48,7 @@ def prepare_env_build_job(pipeline, is_template_test, env_template_version, full
       "ARTIFACT_ID": artifact_id,
       "ENV_TEMPLATE_TEST": "true" if is_template_test else "false",
       "INSTANCES_DIR": "${CI_PROJECT_DIR}/environments",
+      "RUNNER_SCRIPT_TIMEOUT": "20m",
       "envgen_image": "$envgen_image",
       "envgen_args": " -vvv",
       "envgen_debug": "true",
