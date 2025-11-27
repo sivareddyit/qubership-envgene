bd208848febe2f9c8636bed04420e830b6032025
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2025-11-27 12:13:47 +0500

    delete cloud_passport_decryption

--- build_envgene/scripts/process_decryption_mode.py
+++ build_envgene/scripts/process_decryption_mode.py
@@ -1,20 +1,12 @@
-from envgenehelper import crypt, openYaml, logger
 import argparse
 import os
 
-def process_decryption_mode(cred_path, config_path):
-    try:
-        config = openYaml(config_path)
-    except FileNotFoundError:
-        logger.info('configuration/config.yml not found. Cloud passport decryption skipped')
-        return
-    if config == None or not 'cloud_passport_decryption' in config.keys():
-        logger.info('cloud_passport_decryption option is not found in configuration/config.yml. Cloud passport decryption skipped')
-        return
-    if config['cloud_passport_decryption'] != True:
-        logger.info('Cloud passport decryption is skipped')
-        return
+from envgenehelper import crypt
+
+
+def process_decryption_mode(cred_path):
     crypt.decrypt_file(cred_path, ignore_is_crypt=False)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,5 +18,4 @@ if __name__ == "__main__":
     cred_path = os.path.join(base_dir, 'environments', cluster_name, 'cloud-passport', cluster_name + '-creds.yml')
     config_path = os.path.join(base_dir, 'configuration/config.yml')
 
-    process_decryption_mode(cred_path, config_path)
-
+    process_decryption_mode(cred_path)
