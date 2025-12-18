536ff13d94c4d66e507e789e53312898725ecd41
Dias <120464230+dysmon@users.noreply.github.com>
2025-12-18 14:47:15 +0500

    feat: unify logs (#868)
    * chore: trigger pipeline
    
    * feat: centralise logs
    
    * fix: delete needless dict from pipeline parameters
    
    * fix: remove formatting and regex
    
    * feat: log_pipe_params file added
    
    * feat: add dependency
    
    * feat: add dependency
    
    * feat: add dependency
    
    * feat: add dependency
    
    * feat: add dependency
    
    * fix: delete extra attributes
    
    * feat: add script to dockerfile
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * fix: update java version
    
    * feat: test
    
    * feat: test-2
    
    * chore: Update package files [skip ci]
    
    * feat: test-3
    
    * chore: trigger pipeline
    
    * feat: trigger pipeline
    
    * feat: update exception handle
    
    * feat: update exception handle
    
    * feat: centralise logs
    
    * feat: delete extra lines
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: logging updated
    
    * feat: logging updated
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    * feat: centralise logs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build_pipegene/scripts/main.py
+++ build_pipegene/scripts/main.py
@@ -1,31 +1,22 @@
 import click
-import re
 
-from envgenehelper import logger
 from gitlab_ci import build_pipeline
 from validations import validate_pipeline
-from pipeline_parameters import PipelineParametersHandler
+from scripts.utils.pipeline_parameters import PipelineParametersHandler
 
 @click.group(chain=True)
 def gcip():
     pass
-
-def prepare_input_params() -> dict:
-    pipe_params = PipelineParametersHandler()
-    params_log = ("Input parameters are: ")
-    params_log += pipe_params.get_params_str()
-    params_log = params_log = re.sub(r"(CRED_ROTATION_PAYLOAD:\s*)\(.*?\)", r"\1***", params_log, flags=re.DOTALL)
-    logger.info(params_log)
-    return pipe_params.params
 
 @gcip.command("generate_pipeline")
 def generate_pipeline():
     perform_generation()
 
 def perform_generation():
-    params = prepare_input_params()
-    validate_pipeline(params)
-    build_pipeline(params)
+    handler = PipelineParametersHandler()
+    handler.log_pipeline_params()
+    validate_pipeline(handler.params)
+    build_pipeline(handler.params)
 
 if __name__ == "__main__":
     gcip()
