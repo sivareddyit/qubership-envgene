3658f1affb7d44b0eb271ddbaf608d5f37f42f74
Tesma Jose <113982972+tesmarishy@users.noreply.github.com>
2026-01-07 19:02:32 +0530

    fix: added memory monitor (#916)
    * fix: testing with high heap
    
    * fix: added memory monitor

--- build_effective_set_generator_java/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/MemoryMonitor.java
+++ build_effective_set_generator_java/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/MemoryMonitor.java
@@ -1,44 +1,44 @@
-// package org.qubership.cloud.devops.cli.parser;
-// import jakarta.enterprise.context.ApplicationScoped;
-// import io.quarkus.scheduler.Scheduled;
+package org.qubership.cloud.devops.cli.parser;
+import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.scheduler.Scheduled;
 
-// import java.lang.management.*;
-// import java.util.List;
+import java.lang.management.*;
+import java.util.List;
 
-// @ApplicationScoped
-// public class MemoryMonitor {
+@ApplicationScoped
+public class MemoryMonitor {
 
-//     @Scheduled(every = "1s")
-//     void checkMemory() {
+    @Scheduled(every = "1s")
+    void checkMemory() {
 
-//         MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-//         MemoryUsage heap = memoryMXBean.getHeapMemoryUsage();
-//         MemoryUsage nonHeap = memoryMXBean.getNonHeapMemoryUsage();
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heap = memoryMXBean.getHeapMemoryUsage();
+        MemoryUsage nonHeap = memoryMXBean.getNonHeapMemoryUsage();
 
-//         long heapUsed = heap.getUsed();
-//         long nonHeapUsed = nonHeap.getUsed();
+        long heapUsed = heap.getUsed();
+        long nonHeapUsed = nonHeap.getUsed();
 
-//         long directMemory = getDirectMemory();
+        long directMemory = getDirectMemory();
 
-//         System.out.println("===== JVM Memory =====");
-//         System.out.println("Heap Used      : " + toMB(heapUsed) + " MB");
-//         System.out.println("Non-Heap Used  : " + toMB(nonHeapUsed) + " MB");
-//         System.out.println("Direct Memory  : " + toMB(directMemory) + " MB");
-//         System.out.println("Total JVM Used : " +
-//                 toMB(heapUsed + nonHeapUsed + directMemory) + " MB");
-//     }
+        System.out.println("===== JVM Memory =====");
+        System.out.println("Heap Used      : " + toMB(heapUsed) + " MB");
+        System.out.println("Non-Heap Used  : " + toMB(nonHeapUsed) + " MB");
+        System.out.println("Direct Memory  : " + toMB(directMemory) + " MB");
+        System.out.println("Total JVM Used : " +
+                toMB(heapUsed + nonHeapUsed + directMemory) + " MB");
+    }
 
-//     private long getDirectMemory() {
-//         for (BufferPoolMXBean pool :
-//                 ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class)) {
-//             if ("direct".equalsIgnoreCase(pool.getName())) {
-//                 return pool.getMemoryUsed();
-//             }
-//         }
-//         return 0;
-//     }
+    private long getDirectMemory() {
+        for (BufferPoolMXBean pool :
+                ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class)) {
+            if ("direct".equalsIgnoreCase(pool.getName())) {
+                return pool.getMemoryUsed();
+            }
+        }
+        return 0;
+    }
 
-//     private long toMB(long bytes) {
-//         return bytes / (1024 * 1024);
-//     }
-// }
+    private long toMB(long bytes) {
+        return bytes / (1024 * 1024);
+    }
+}
