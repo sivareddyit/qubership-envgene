59d9229b6379a57fd8546f2df10fd0f06ed6bef7
Andrei Rudchenko <104736077+andyroode@users.noreply.github.com>
2026-01-29 12:06:13 +0300

    feat: Added new separators for ENV_NAMES (#969)
    * chore: Added separators to Env_Names
    
    * chore: Added new delimeters to generate env matrix script

--- github_workflows/instance-repo-pipeline/.github/scripts/generate_env_matrix.sh
+++ github_workflows/instance-repo-pipeline/.github/scripts/generate_env_matrix.sh
@@ -5,7 +5,10 @@ if [ -z "$ENV_NAMES" ]; then
     exit 1
 fi
 
-env_matrix=$(echo "$ENV_NAMES" | jq -R -s -c 'split(",") | map(select(. != "" and . != null)) | map(gsub("^\\s+|\\s+$"; "")) | map(select(. != ""))')
+# Replace literal \n with comma
+ENV_NAMES="${ENV_NAMES//\\n/,}"
+
+env_matrix=$(echo "$ENV_NAMES" | jq -R -s -c 'split(",") | map(split(";")) | flatten | map(split(" ")) | flatten | map(select(. != "" and . != null)) | map(gsub("^\\s+|\\s+$"; "")) | map(select(. != ""))')
 
 echo "env_matrix=$env_matrix" >>$GITHUB_OUTPUT
 echo "Generated matrix: $env_matrix"
