e10f8a52772444ca8f4b69d02ce68017ebf74c98
Tesma Jose <113982972+tesmarishy@users.noreply.github.com>
2026-02-11 17:26:52 +0530

    fix: added logging (#1012)
    * fix: added logging
    
    * fix: added entry script
    
    * fix: added permissions

--- scripts/utils/entrypoint.sh
+++ scripts/utils/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-echo "am inside entrypoint"
-export JAVA_OPTIONS="$JAVA_OPTIONS $JAVA_MEM_INITIAL $JAVA_MEM_MAX $THREAD_COUNT"
+
+echo "Starting entrypoint..."
+
+
+[ -n "$JAVA_MEM_INITIAL" ] && JAVA_OPTIONS="$JAVA_OPTIONS $JAVA_MEM_INITIAL"
+[ -n "$JAVA_MEM_MAX" ] && JAVA_OPTIONS="$JAVA_OPTIONS $JAVA_MEM_MAX"
+[ -n "$THREAD_COUNT" ] && JAVA_OPTIONS="$JAVA_OPTIONS $THREAD_COUNT"
+
+
+echo "Final JAVA_OPTIONS: $JAVA_OPTIONS"
+
+export JAVA_OPTIONS
+
 exec /deployments/run-java.sh "$@"
